a37635aa0c390c23f6c52111cb75d34d8dd70d67
fiveseven <fiveseven.lambda@gmail.com>
2023-11-14 14:09:25 +0900

    clone / delete のタイミングが難しいぜ

--- gc_cranelift/src/main.rs
+++ gc_cranelift/src/main.rs
@@ -1,4 +1,9 @@
-use std::{mem::ManuallyDrop, rc::Rc};
+use std::{
+    collections::HashMap,
+    fmt::{self, Debug, Formatter},
+    mem::ManuallyDrop,
+    rc::Rc,
+};
 
 use cranelift::prelude::*;
 use cranelift_jit::{JITBuilder, JITModule};
@@ -13,15 +18,19 @@ extern "C" fn new_leaf(value: i32) -> *const Tree {
     Rc::into_raw(Tree::Leaf(value).into())
 }
 unsafe extern "C" fn add_child(children: &mut Vec<Rc<Tree>>, tree: *const Tree) {
-    Rc::increment_strong_count(tree);
     children.push(Rc::from_raw(tree));
 }
 extern "C" fn new_node(children: &mut Vec<Rc<Tree>>) -> *const Tree {
     let children = std::mem::take(children);
     Rc::into_raw(Tree::Node(children).into())
 }
+unsafe extern "C" fn clone_tree(ptr: *const Tree) {
+    Rc::increment_strong_count(ptr);
+}
 unsafe extern "C" fn delete_tree(ptr: *const Tree) {
-    Rc::from_raw(ptr);
+    if !ptr.is_null() {
+        Rc::from_raw(ptr);
+    }
 }
 unsafe extern "C" fn print_tree(ptr: *const Tree) {
     let tree = ManuallyDrop::new(Rc::from_raw(ptr));
@@ -47,7 +56,102 @@ fn tree2string(tree: &Rc<Tree>) -> String {
     }
 }
 
+enum Expr {
+    Var(&'static str),
+    Leaf(i32),
+    Node(Vec<Expr>),
+}
+impl Debug for Expr {
+    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+        match self {
+            Expr::Var(name) => write!(f, "{name}"),
+            Expr::Leaf(value) => write!(f, "{value}"),
+            Expr::Node(children) => {
+                write!(
+                    f,
+                    "[{}]",
+                    children
+                        .iter()
+                        .map(|child| format!("{child:?}"))
+                        .collect::<Vec<_>>()
+                        .join(",")
+                )
+            }
+        }
+    }
+}
+impl Expr {
+    fn compile(
+        &self,
+        builder: &mut FunctionBuilder,
+        variables: &mut HashMap<&str, Variable>,
+        (new_leaf_sig, new_leaf_ptr): (codegen::ir::SigRef, Value),
+        (clone_tree_sig, clone_tree_ptr): (codegen::ir::SigRef, Value),
+        children: Value,
+        (add_child_sig, add_child_ptr): (codegen::ir::SigRef, Value),
+        (new_node_sig, new_node_ptr): (codegen::ir::SigRef, Value),
+    ) -> Value {
+        match *self {
+            Expr::Leaf(value) => {
+                let value = builder.ins().iconst(types::I32, Into::<i64>::into(value));
+                let inst = builder
+                    .ins()
+                    .call_indirect(new_leaf_sig, new_leaf_ptr, &[value]);
+                builder.inst_results(inst)[0]
+            }
+            Expr::Var(name) => {
+                let variable = *variables.get(name).expect("undefined variable");
+                let value = builder.use_var(variable);
+                builder
+                    .ins()
+                    .call_indirect(clone_tree_sig, clone_tree_ptr, &[value]);
+                value
+            }
+            Expr::Node(ref children_expr) => {
+                for child_expr in children_expr {
+                    let child = child_expr.compile(
+                        builder,
+                        variables,
+                        (new_leaf_sig, new_leaf_ptr),
+                        (clone_tree_sig, clone_tree_ptr),
+                        children,
+                        (add_child_sig, add_child_ptr),
+                        (new_node_sig, new_node_ptr),
+                    );
+                    builder
+                        .ins()
+                        .call_indirect(add_child_sig, add_child_ptr, &[children, child]);
+                }
+                let inst = builder
+                    .ins()
+                    .call_indirect(new_node_sig, new_node_ptr, &[children]);
+                builder.inst_results(inst)[0]
+            }
+        }
+    }
+}
+
 fn main() {
+    let program = vec![
+        (Some("a"), Expr::Leaf(10)),
+        (Some("b"), Expr::Leaf(20)),
+        (
+            Some("a"),
+            Expr::Node(vec![
+                Expr::Node(vec![Expr::Var("a"), Expr::Var("b")]),
+                Expr::Var("b"),
+            ]),
+        ),
+        (None, Expr::Var("a")),
+        (None, Expr::Var("b")),
+    ];
+    for (var, expr) in &program {
+        match var {
+            Some(name) => println!("{name} <- {expr:?}"),
+            None => println!("print {expr:?}"),
+        }
+    }
+
     let mut trees = Vec::new();
 
     let jit_builder = JITBuilder::new(cranelift_module::default_libcall_names()).unwrap();
@@ -60,7 +164,6 @@ fn main() {
     let mut builder = FunctionBuilder::new(&mut ctx.func, &mut fn_builder_ctx);
 
     let block = builder.create_block();
-    builder.append_block_params_for_function_params(block);
     builder.switch_to_block(block);
 
     let new_leaf_ptr = builder
@@ -91,15 +194,20 @@ fn main() {
         sig.returns.push(AbiParam::new(ptr_ty));
         sig
     });
-    let delete_tree_ptr = builder.ins().iconst(
+    let clone_tree_ptr = builder.ins().iconst(
         ptr_ty,
-        TryInto::<i64>::try_into(delete_tree as usize).unwrap(),
+        TryInto::<i64>::try_into(clone_tree as usize).unwrap(),
     );
-    let delete_tree_sig = builder.import_signature({
+    let clone_tree_sig = builder.import_signature({
         let mut sig = module.make_signature();
         sig.params.push(AbiParam::new(ptr_ty));
         sig
     });
+    let delete_tree_ptr = builder.ins().iconst(
+        ptr_ty,
+        TryInto::<i64>::try_into(delete_tree as usize).unwrap(),
+    );
+    let delete_tree_sig = clone_tree_sig;
     let print_tree_ptr = builder.ins().iconst(
         ptr_ty,
         TryInto::<i64>::try_into(print_tree as usize).unwrap(),
@@ -110,39 +218,51 @@ fn main() {
         .ins()
         .iconst(ptr_ty, &mut trees as *mut Vec<Rc<Tree>> as i64);
 
-    let var0 = Variable::new(0);
-    builder.declare_var(var0, ptr_ty);
-    let one = builder.ins().iconst(types::I32, 1);
-    let leaf_one_inst = builder
-        .ins()
-        .call_indirect(new_leaf_sig, new_leaf_ptr, &[one]);
-    let leaf_one = builder.inst_results(leaf_one_inst)[0];
-    builder.def_var(var0, leaf_one);
-
-    let var1 = Variable::new(1);
-    builder.declare_var(var1, ptr_ty);
-    builder
-        .ins()
-        .call_indirect(add_child_sig, add_child_ptr, &[children, leaf_one]);
-    builder
-        .ins()
-        .call_indirect(add_child_sig, add_child_ptr, &[children, leaf_one]);
-    let node_one_one_inst = builder
-        .ins()
-        .call_indirect(new_node_sig, new_node_ptr, &[children]);
-    let node_one_one = builder.inst_results(node_one_one_inst)[0];
-    builder
-        .ins()
-        .call_indirect(print_tree_sig, print_tree_ptr, &[node_one_one]);
-    builder
-        .ins()
-        .call_indirect(delete_tree_sig, delete_tree_ptr, &[leaf_one]);
-    builder
-        .ins()
-        .call_indirect(print_tree_sig, print_tree_ptr, &[node_one_one]);
-    builder
-        .ins()
-        .call_indirect(delete_tree_sig, delete_tree_ptr, &[node_one_one]);
+    let mut variables = HashMap::new();
+    for (var, expr) in program {
+        let value = expr.compile(
+            &mut builder,
+            &mut variables,
+            (new_leaf_sig, new_leaf_ptr),
+            (clone_tree_sig, clone_tree_ptr),
+            children,
+            (add_child_sig, add_child_ptr),
+            (new_node_sig, new_node_ptr),
+        );
+        match var {
+            Some(name) => {
+                let index = variables.len();
+                let variable = *variables.entry(name).or_insert_with(|| {
+                    let variable = Variable::new(index);
+                    builder.declare_var(variable, ptr_ty);
+                    let null = builder
+                        .ins()
+                        .iconst(ptr_ty, std::ptr::null::<Tree>() as i64);
+                    builder.def_var(variable, null);
+                    variable
+                });
+                let old_value = builder.use_var(variable);
+                builder
+                    .ins()
+                    .call_indirect(delete_tree_sig, delete_tree_ptr, &[old_value]);
+                builder.def_var(variable, value);
+            }
+            None => {
+                builder
+                    .ins()
+                    .call_indirect(print_tree_sig, print_tree_ptr, &[value]);
+                builder
+                    .ins()
+                    .call_indirect(delete_tree_sig, delete_tree_ptr, &[value]);
+            }
+        }
+    }
+    for (_, variable) in variables {
+        let value = builder.use_var(variable);
+        builder
+            .ins()
+            .call_indirect(delete_tree_sig, delete_tree_ptr, &[value]);
+    }
 
     builder.ins().return_(&[]);
     builder.seal_block(block);
